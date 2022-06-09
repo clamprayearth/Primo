aa537ad4b76edb64ecce94d848970b11c1b5fdc1
“clamprayearth” <azalu1984@yahoo.com>
2022-06-09 03:32:22 -0300

    Com modex

--- primo.c
+++ primo.c
@@ -4,9 +4,11 @@
 #include <time.h>
 #include <unistd.h>
 
-int number, numTid, divisao, pronto;
+int number, numTid, divisao, count;
 clock_t start, end;
 double cpu_time_used;
+pthread_mutex_t lock;
+int primos[1000];
 
 int calculIndex(int atualIndex, int numLin, int numCol){
 	/*Esta função calcula o valor do indice para cada coluna referente a matriz(thread, intervalo)*/
@@ -22,6 +24,7 @@ int calculIndex(int atualIndex, int numLin, int numCol){
 
 void *funcao(void *num){
 	/* Esta funcao faz o calculo dos numeros primos do intervalo dado. */
+	pthread_mutex_lock(&lock);
 	printf("\nThread iniciada\n");
 	
 	int *intervalo = (int *)num;
@@ -34,20 +37,23 @@ void *funcao(void *num){
 				break;
 			}
 		}
-		if((isPrimo == 1) && (intervalo[i] != 1) && (intervalo[i] != 0))
-			printf(" %d ", intervalo[i]);
+		if((isPrimo == 1) && (intervalo[i] != 1) && (intervalo[i] != 0)){
+			primos[count] = intervalo[i];
+			count++;
+		}
 		isPrimo = 1;
 	}	
 	
 	free(intervalo);
-	pronto = 1;
 	printf("\nthread terminou");
+	pthread_mutex_unlock(&lock);
 	pthread_exit(NULL);
 }
 
 int main (int argc, char *argv []){
 	start = clock(); //<-- Inicio do cronometro para calcular o tempo de execução
 	
+	count = 0;
 	//number = O maior número do intervalo; numTid = numero de thread 
 	number = atoi (argv[1]);
 	numTid = atoi (argv[2]);
@@ -70,15 +76,17 @@ int main (int argc, char *argv []){
 		//alocação dinamica de vetor para o intervalo
 		int *intervalo = malloc (divisao * sizeof(int));
 		
+		if (pthread_mutex_init(&lock, NULL) != 0)
+    	{
+        printf("\n mutex init failed\n");
+        return 1;
+    	}
 	
 		for(int j=0; j<divisao; j++, maximo++){
 			intervalo[j] = maximo;	
 		}
 		
-		//Semaforo para as treads
-		pronto = 0;
 		status = pthread_create(&tid[i], &attr, funcao, intervalo);
-		while (!pronto) {}
 		
 		if (status != 0) {
 			printf("Oops. pthread create returned error code %d\n", status);
@@ -90,11 +98,16 @@ int main (int argc, char *argv []){
 		pthread_join (tid[i], NULL);	
 	}
 	free(tid);
-	end = clock(); //<-- Fim do cronometro para calcular o tempo de execução
 	
+	printf("\n");
+	for(int i=0; i<count; i++) {
+		printf("%d ", primos[i]);	
+	}
+	end = clock(); //<-- Fim do cronometro para calcular o tempo de execução
 	//Calculo do tempo final
 	cpu_time_used = ((double) (end - start)) / CLOCKS_PER_SEC;
 	
-	printf("\nTempo de exec: %f", cpu_time_used);
+	printf("\nTempo de exec: %f sec\n", cpu_time_used);
+	pthread_mutex_destroy(&lock);
 	return 0;
 }
